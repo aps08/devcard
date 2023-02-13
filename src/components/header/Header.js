8abee5dfe3a0c18e3fdd397b49c1689bf2c52a6d
aps08 <anoopprsingh.social@gmail.com>
2023-02-13 23:54:42 +0530

    header added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,19 +1,40 @@
 import '../header/Header.css';
 import logo from '../../assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
+  const loggedIn = false;
+
   return (
     <div className="header">
       <div className="branding">
         <img className="brand_logo" src={logo} alt="React Image" />
         <span className="brand_name">Devcards</span>
       </div>
-      {/* <div className="navbar">
-        <a className="active" href="/home">Home</a>
-        <a href="/news">News</a>
-        <a href="/contact">Contact</a>
-        <a href="/about">About</a>
-      </div> */}
+      <div className="navbar">
+        <ul className="navbar-nav">
+          <NavLink to="home">
+            <li className="nav-item">Home</li>
+          </NavLink>
+          {!loggedIn && (
+            <NavLink to="demo">
+              <li className="nav-item">Demo</li>
+            </NavLink>
+          )}
+          <NavLink to="about">
+            <li className="nav-item">About</li>
+          </NavLink>
+          {loggedIn && (
+            <NavLink to="dashboard">
+              <li className="nav-item">Dashboard</li>
+            </NavLink>
+          )}
+        </ul>
+        {!loggedIn && (
+          <button className="nav-item nav-btn">Sign up for free</button>
+        )}
+        {!loggedIn && <button className="nav-item nav-btn">Sign in</button>}
+      </div>
     </div>
   );
 };
