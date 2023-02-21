779fdf01b60e723bae56ad47779db6f7b1ab171e
aps08 <anoopprsingh.social@gmail.com>
2023-02-21 22:19:30 +0530

    footer completed and adding home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,30 @@
 import './Home.css';
 
 const Home = () => {
-  return <>Home Page</>;
+  return (
+    <>
+      <section className="tag-card">
+        <div className="tag">
+          <h3>
+            Impress with Devcards.
+            <br />
+            Showcase expertise
+            <br />
+            professionally.
+          </h3>
+          <p>
+            Stand out from the competition with our high-quality business cards
+            designed to showcase your skills and expertise as a developer.
+          </p>
+          <button className="l-text"></button>
+        </div>
+        <div className="card">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
+          imperdiet feugiat lectus ac venenatis.
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Home;
