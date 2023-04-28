6b9fd4038fd63b6cb62a791fdbb00b3013bcb843
aps08 <anoopprsingh.social@gmail.com>
2023-04-28 23:53:27 +0530

    order component completed

---
+++ src/components/purchasehistory/Orderinfo.js
@@ -0,0 +1,42 @@
+function Orderinfo(props) {
+  const date = new Date(props.ordered);
+  const options = { month: "long", day: "numeric", year: "numeric" };
+  const formattedDate = date.toLocaleDateString("en-US", options);
+  return (
+    <div className="order_card">
+      <div className="order_card_top">
+        <div className="order_block">
+          <div className="order_info">
+            <p className="label_order">Order placed</p>
+            <p className="label_info">{formattedDate}</p>
+          </div>
+          <div className="order_info">
+            <p className="label_order">Name</p>
+            <p className="label_info">{props.name}</p>
+          </div>
+        </div>
+        <div className="order_block">
+          <div className="order_info">
+            <p className="label_order">Status</p>
+            <p className="label_info">{props.status}</p>
+          </div>
+          <div className="order_info">
+            <p className="label_order">type</p>
+            <p className="label_info">{props.type}</p>
+          </div>
+        </div>
+        <div className="order_block">
+          <div className="order_info">
+            <p className="label_order">ORDER ID</p>
+            <p className="label_info">{props.orderid}</p>
+          </div>
+          <div className="order_info">
+            <button style={{ fontSize: "1rem", margin: "0" }}>Raise issue</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Orderinfo;
--- src/components/purchasehistory/Purchasehistory.js
+++ src/components/purchasehistory/Purchasehistory.js
@@ -1,43 +1,35 @@
+import Orderinfo from "./Orderinfo";
 import "./Purchasehistory.css";
 
 function Purchasehistory() {
   const date = new Date("2023-02-27");
   const options = { month: "long", day: "numeric", year: "numeric" };
   const formattedDate = date.toLocaleDateString("en-US", options);
+  const orders = [
+    {
+      ordered: "2023-02-27",
+      name: "Gold flex",
+      orderid: "1234567",
+      status: "Shipping",
+      type: "Purcchase"
+    }
+  ];
   console.log(formattedDate);
   return (
     <div className="purchasehistory">
       <h2 style={{ marginBottom: "1rem" }}>Your orders</h2>
-      <div className="order_card">
-        <div className="order_card_top">
-          <div>
-            <p className="label_order">Order placed</p>
-            <p className="order_date">{formattedDate}</p>
-          </div>
-          <div>
-            <p className="label_order">type</p>
-            <p className="order_date">Purchase</p>
-          </div>
-          <div>
-            <p className="label_order">ORDER IB</p>
-            <p className="order_date">sdjsndjnsjnsjcnsjcv </p>
-          </div>
-        </div>
-        <div className="order_card_body">
-          <div>
-            <p className="label_order">Status</p>
-            <p className="order_date">In porgress</p>
-          </div>
-          <div>
-            <p className="label_order">shipped to</p>
-            <p className="order_date">Anoop</p>
-          </div>
-          <div>
-            <p className="label_order">Credits spent</p>
-            <p className="order_date">3</p>
-          </div>
-        </div>
-      </div>
+      {orders.length > 0 &&
+        orders.map((order) => (
+          <Orderinfo
+            key={order.orderid}
+            ordered={order.ordered}
+            name={order.name}
+            orderid={order.orderid}
+            status={order.status}
+            type={order.type}
+          />
+        ))}
+      {orders.length === 0 && <p style={{ textAlign: "center" }}>No order information found.</p>}
     </div>
   );
 }
